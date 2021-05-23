f40c5a61f9e1b2d1e2929bcc7c1ef847eac94d35
leila Zintsem a moute <leilazintsemamoute@gmail.com>
2021-05-23 18:59:32 -0500

    List all available api routes

--- app.py
+++ app.py
@@ -23,3 +23,19 @@ Base.prepare(engine, reflect=True)
 # Save reference to the tables
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+
+#################################################
+# Flask Routes
+#################################################
+"""List all available api routes."""
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start (enter as YYYY-MM-DD)<br/>"
+        f"/api/v1.0/start/end (enter as YYYY-MM-DD/YYYY-MM-DD)"
+    )
